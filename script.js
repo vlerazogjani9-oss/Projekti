c624ef0c51dab672854d280a0da241a88fc98376
vlerazogjani9-oss <vlerazogjani9@gmail.com>
2025-12-11 23:42:39 +0100

    Shtova elemente te reja ne homepage dhe ndryshime tek loginform.html dhe script.js

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ document.getElementById("registerBtn").addEventListener("click", function() {
     window.location.href = "registerform.html";  
 });
 
+document.getElementById("registerLink").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href = "registerform.html";  
+});
+
+
 document.getElementById("language-select").addEventListener("change", function() {
     const lang = this.value;
     if(lang === "english") {
-        alert("Gjuha u ndryshua në English!"); // ose mund të ridrejtohesh në versionin English
+        alert("Gjuha u ndryshua në English!"); 
     } else if(lang === "shqip") {
         alert("Gjuha u ndryshua në Shqip!");
     }
 });
+
+
